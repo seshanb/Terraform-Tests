8fcb0e5f0ca6d617daa8cef6cb9e85265392c779
mfallihee <32306654+mfallihee@users.noreply.github.com>
2023-06-01 11:57:04 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+# Specify the provider and access details
+variable "security_Group_Name"{
+  description = "The name for the security group to create"
+}
+
+variable "subnet_ID"{
+  description = "The ID for the existing subnet"
+}
