88a514d1f0e65efbf1a76b9366f88cdcadcad3f2
mfallihee <32306654+mfallihee@users.noreply.github.com>
2023-06-16 07:52:29 -0600

    Update check_inspector.sh

--- scripts/check_inspector.sh
+++ scripts/check_inspector.sh
@@ -15,8 +15,8 @@ endpoint_count=`aws inspector list-assessment-templates --region ${region_name} 
 # If the value is greater than 0, that means Inspector is enabled in the region
 if [ ${endpoint_count} -gt 0 ]; then
   result='true'
-  printf '{"endpoint_exists": "%s", "regions": "%s"}\n' "$result" "$regions"
+  printf '{"endpoint_exists": "%s", "regions": "%s"}\n' "$result" "regions"
 else
   result='false'
-  printf '{"endpoint_exists": "%s", "regions": "%s"}\n' "$result" "$regions"
+  printf '{"endpoint_exists": "%s", "regions": "%s"}\n' "$result" "regions"
 fi
