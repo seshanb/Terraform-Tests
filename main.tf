61747c27b14f84e6676b1b045a7baff530ae5b09
mfallihee <32306654+mfallihee@users.noreply.github.com>
2023-06-16 06:30:41 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_region" "current" {}
 
 data "external" "inspector_exists" {
   program = [
-    "/bin/bash",
+    "/bin/sh",
     "${path.module}/scripts/check_inspector.sh",
     "${data.aws_region.current.name}"
   ]
