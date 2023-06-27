ae38d3c6bf960b50fc48193999b5709dca3f7e28
seshanb <125367985+seshanb@users.noreply.github.com>
2023-06-27 17:48:59 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-# Specify the provider and access details
-variable "security_Group_Name"{
-  description = "The name for the security group to create"
-}
-
-variable "subnet_ID"{
-  description = "The ID for the existing subnet"
-}
